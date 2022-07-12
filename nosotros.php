6e63b5e663bfb3c2bc657bd507a539c1ae923eee
EmilianoGDY <emilianoo9g@gmail.com>
2022-07-12 11:55:21 -0300

    primer commit con muchos avances

---
+++ nosotros.php
@@ -0,0 +1,13 @@
+<?php include("template/cabecera.php");?>
+
+
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Hola soy Emiliano Oscar Godoy</p>
+    <hr class="my-2">
+   
+</div>
+
+
+
+<?php include("template/pie.php");?>
