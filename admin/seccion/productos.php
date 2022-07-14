ceed9147179e6cb2821c04ba97232f42e5fdd758
EmilianoGDY <emilianoo9g@gmail.com>
2022-07-14 17:45:00 -0300

    funcion agregar de admin listo

--- admin/seccion/productos.php
+++ admin/seccion/productos.php
@@ -1,4 +1,36 @@
-<?php include("../template/cabecera.php")?>
+<?php include("../template/cabecera.php");?>
+<?php 
+
+$txtID = (isset($_POST['txtID']))?$_POST['txtID']:"";
+$txtNombre = (isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
+$txtIMG = (isset($_FILES['txtIMG']['name']))?$_FILES['txtIMG']['name']:"";
+$accion = (isset($_POST['accion']))?$_POST['accion']:"";
+
+include("../config/bd.php");
+
+
+switch($accion){
+    case"Agregar":
+        
+        $sentenciaSQL= $conexion->prepare("INSERT INTO libros (nombre, imagen) VALUES (:nombre,:imagen);");
+        $sentenciaSQL->bindParam(':nombre',$txtNombre);
+        $sentenciaSQL->bindParam(':imagen',$txtIMG);
+        $sentenciaSQL->execute();
+        break;
+    case"Modificar":
+        echo "Presionado boton Modificar";
+        break;
+    case"Cancelar":
+        echo "Presionado boton Cancelar";
+        break;
+
+}
+$sentenciaSQL= $conexion->prepare("SELECT * FROM libros");
+$sentenciaSQL->execute();
+$listaLibros=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+
+
+?>
 
 <div class="col-md-5">
 
@@ -10,30 +42,30 @@
         <div class="card-body">
         <form method="POST" enctype="multipart/form-data">
 
-<div class = "form-group">
-<label for="txtID">ID:</label>
-<input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
-</div>
+            <div class = "form-group">
+                <label for="txtID">ID:</label>
+                <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
+            </div>
 
-<div class = "form-group">
-<label for="txtNombre">Nombre:</label>
-<input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre">
-</div>
+            <div class = "form-group">
+                <label for="txtNombre">Nombre:</label>
+                <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre">
+            </div>
 
-<div class = "form-group">
-<label for="txtIMG">Imagen:</label>
-<input type="file" class="form-control" name="txtIMG" id="txtIMG" placeholder="Imagen">
-</div>
+            <div class = "form-group">
+                <label for="txtIMG">Imagen:</label>
+                <input type="file" class="form-control" name="txtIMG" id="txtIMG" placeholder="Imagen">
+            </div>
 
 
 
-<div class="btn-group" role="group" aria-label="">
-    <button type="button" class="btn btn-success">Agregar</button>
-    <button type="button" class="btn btn-warning">Modificar</button>
-    <button type="button" class="btn btn-info">Cancelar</button>
-</div>
+            <div class="btn-group" role="group" aria-label="">
+                <button type="submit" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
+                <button type="submit" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
+                <button type="submit" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
+            </div>
 
-</form>
+        </form>
         </div>
 
        
@@ -44,8 +76,27 @@
     
 </div>
 <div class="col-md-7">
-    Tabla de Libros
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Imagen</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach($listaLibros as $libro)  {?>
+            <tr>
+                <td><?php echo $libro['id']; ?></td>
+                <td><?php echo $libro['nombre']; ?></td>
+                <td><?php echo $libro['imagen']; ?></td>
+                <td>Seleccionar | Borrar</td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
 </div>
 
 
-<?php include("../template/pie.php")?>
+<?php include("../template/pie.php");?>
